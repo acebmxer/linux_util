731b4d799238a0212e5f89ad727f0df9f6629151
acebmxer <nick.pozza1@gmail.com>
2026-03-28 19:25:27 -0400

    fix(xrdp): set XDG env vars for KDE Plasma RDP sessions

--- lib/installers/xrdp.sh
+++ lib/installers/xrdp.sh
@@ -23,7 +23,13 @@ install_xrdp() {
 
                 info "Configuring xrdp to launch KDE Plasma X11 session..."
                 run_as_root sed -i '/^test -x \/etc\/X11\/Xsession/d; /^exec \/bin\/sh \/etc\/X11\/Xsession/d' /etc/xrdp/startwm.sh
-                echo "exec /usr/bin/startplasma-x11" | run_as_root tee -a /etc/xrdp/startwm.sh > /dev/null
+                run_as_root tee -a /etc/xrdp/startwm.sh > /dev/null <<'STARTWM'
+export XDG_SESSION_TYPE=x11
+export XDG_SESSION_DESKTOP=KDE
+export XDG_CURRENT_DESKTOP=KDE
+export DESKTOP_SESSION=plasma
+exec /usr/bin/startplasma-x11
+STARTWM
 
                 info "Granting xrdp access to SSL certificates..."
                 run_as_root adduser xrdp ssl-cert
